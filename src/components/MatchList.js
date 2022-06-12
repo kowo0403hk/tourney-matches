21b172a828bdcb4910c01ec986d990fc1df91fac
Alex Cheung <alexc.sfu@gmail.com>
2022-06-12 00:16:28 -0700

    created components folder, added components files, updated index.css and App.css, as well as App.css

---
+++ src/components/MatchList.js
@@ -0,0 +1,10 @@
+function MatchList() {
+  return (
+    <section className='PlayerList MatchList'>
+      <h1>Match List</h1>
+      {/* Matches will be shown here */}
+    </section>
+  );
+};
+
+export default MatchList;
