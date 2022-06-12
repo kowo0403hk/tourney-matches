d3b95de4126a8784aac987a634aeb88cf6d75aa6
Alex Cheung <alexc.sfu@gmail.com>
2022-06-12 01:15:29 -0700

    added data folders for contain players and match information, also added helpers folder to contain helpers functions. Finished importing data to PlayerList.js

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,6 +1,7 @@
 import Match from './Match';
+import matchData from '../data/matchData';
 
-function MatchList() {
+function MatchList(props) {
   return (
     <section className='PlayerList MatchList'>
       <h1>Match List</h1>
